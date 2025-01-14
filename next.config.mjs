1efdd43323f5cc23c69530331d5c5e54e846d124
The-3rio <craftmenlab@gmail.com>
2025-01-14 20:11:15 +0530

    route

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
           {
             source: "/",
-            destination: "/main_dashboard",
+            destination: "/authentication",
             permanent: true,
           },
 
